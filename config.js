f38d4a4bdaaccf27ba9dac875cb0244689ed51c8
shanmufti <shan.mufti@gmail.com>
2020-12-22 10:41:46 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  pathPrefix: "/shanweb",
+  siteTitle: 'Gatsby Starter Prologue', // <title>
+  manifestName: 'Prologue',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-prologue/`, // This path is subpath of your hosting https://domain/portfolio
+  authorName: 'Shan',
+  heading: 'Computer Engineer',
+  // social
+  socialLinks: [
+    
+      {
+        icon: 'fa-linkedin',
+        name: 'LinkedIn',
+        url: 'https://linkedin.com/in/shanmufti',
+      },
+   {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/shanmufti95',
+    },
+    {
+      icon: 'fa-facebook',
+      name: 'Facebook',
+      url: 'https://facebook.com/shanmufti',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:shan.mufti@gmail.com',
+    },
+  ],
+};
